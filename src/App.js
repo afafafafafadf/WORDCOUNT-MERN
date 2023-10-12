97daec28dfaea3ed8141c756a9e0b91aaf8df15a
DankDio <61165279+afafafafafadf@users.noreply.github.com>
2023-10-12 11:06:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Textform from "./Textform";
+function App() {
+  return (
+    <div className="container">
+      <Textform heading = "Wordcount"/>
+    </div>
+  );
+}
+
+export default App;
